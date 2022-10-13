1cdb9b7ad2c2008c6d41bc7c51ee2288e61c6bea
VladKapusta <vladkapysta96@gmail.com>
2022-10-13 14:21:57 +0300

    адаптивна верстка, фдаптивні зображення

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,20 @@
 (() => {
   const refs = {
+    openModalBtn: document.querySelector("[data-mobail-open]"),
+    closeModalBtn: document.querySelector("[data-mobail-close]"),
+    modal: document.querySelector("[data-mobail]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+  }
+})();
+
+(() => {
+  const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
@@ -12,3 +27,4 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
