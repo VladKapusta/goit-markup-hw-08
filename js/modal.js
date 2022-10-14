1c46848c4d276594256cc538e73c639a69d28bf6
VladKapusta <vladkapysta96@gmail.com>
2022-10-14 16:06:30 +0300

    виправляє модалку, додає скрипт мобільного меню

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,4 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-mobail-open]"),
-    closeModalBtn: document.querySelector("[data-mobail-close]"),
-    modal: document.querySelector("[data-mobail]"),
-  };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-open");
-  }
-})();
 
 (() => {
   const refs = {
@@ -24,6 +11,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open")
     refs.modal.classList.toggle("is-hidden");
   }
 })();
